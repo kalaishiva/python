375b7105977bed4965b74c9940910922779cbd0f
designhappily <65686852+shivakalai@users.noreply.github.com>
2026-02-02 12:32:32 +0100

    conditional examples

---
+++ 03_Conditionals/mini_story1.py
@@ -0,0 +1,4 @@
+Kettle_boiled = True
+
+if Kettle_boiled:
+    print(f"Kettle done : Time to make Chai")
---
+++ 03_Conditionals/snack_suggestion.py
@@ -0,0 +1,6 @@
+snack = input("Enter your snack : ").lower()
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great choice!, we will serve your {snack} soon")
+else:
+    print(f"Sorry, we only have cookies and samosa")
