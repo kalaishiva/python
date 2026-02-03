899fa1056b3f5eb4b75176a83c8ddc349a7a76cb
designhappily <65686852+shivakalai@users.noreply.github.com>
2026-02-03 12:50:48 +0100

    loop token dispenser added

---
+++ 04_Loop/token_dispenser.py
@@ -0,0 +1,5 @@
+# Use for loop to generate token number from 1 to 10 using range().
+# print: "Serving chai to Token # {number}"
+
+for token in range(1,11): # range are not inclusive so 1 to 11
+    print(f" Serving chai to Token {token}")
