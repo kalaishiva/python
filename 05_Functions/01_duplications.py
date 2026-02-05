d7b5471ce50d497a04b1c73118627fa45c7584b6
designhappily <65686852+shivakalai@users.noreply.github.com>
2026-02-05 10:22:16 +0100

    Function-duplication and complex added

---
+++ 05_Functions/01_duplications.py
@@ -0,0 +1,14 @@
+# want to print customer name with the chai type
+# Task:
+# Write a function with print_order(name, chai_type)
+# call it multiple times for the different customer
+
+# def is called as definition
+
+
+def print_order(name, chai_type):
+    print(f"{name} ordered {chai_type} chai !") 
+
+print_order("Kalai", "lemon")
+print_order("Shivani", "lemon")
+print_order("Vani", "ginger")
---
+++ 05_Functions/02.complex.py
@@ -0,0 +1,29 @@
+# creating a monthly report for a cafe sales. 
+# Instead of putting all logic in one place, break it down.
+# Task:
+# 1. Write a function generate_report that calls:
+#     * fetch_sales
+#     * filter_valid_sales
+#     * summarize_data
+
+
+def fetch_sales():
+    print(f"Fetching the sales data")
+    
+    
+def filter_valid_sales():
+    print(f"Filtering valid sales data")
+    
+    
+def summarize_data():
+    print(f"Summarizing the sales data")
+
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+    print("Report is ready")
+
+
+generate_report()
