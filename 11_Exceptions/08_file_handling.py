92e2a7a6998ebe0b26b02518768a03e9daf2b7e4
designhappily <65686852+shivakalai@users.noreply.github.com>
2026-02-17 19:17:46 +0100

    File handling added

---
+++ 11_Exceptions/08_file_handling.py
@@ -0,0 +1,10 @@
+#file = open("order.txt", "w")
+#try:
+ #   file.write("Masala chai - 2 cups")
+#finally:
+ #   file.close()
+
+# Latest 
+
+with open("order.txt", "w") as file:
+    file.write("Ginger tea - 5 cups")
